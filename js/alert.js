bebd341a6f8bbde7cbe6eba33148808792f535f3
Ssentamu Jordan <ssentamujordan@gmail.com>
2022-09-07 13:35:30 +0300

    add an interaction prompt

--- js/alert.js
+++ js/alert.js
@@ -1,1 +1,17 @@
-alert("I am JavaScript!");
+"use script"
+// Javascript introdution
+console.log("I am JavaScript!");
+// Variable 
+const name = "John";
+const admin = "John";
+console.log(admin);
+
+const ourPlanet = "Earth";
+const CurrentUserName = "Jordan";
+
+// data-types
+
+// using prompt
+const yourName = prompt ('What is your Name ?');
+console.log(yourName)
+
