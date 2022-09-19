fc384cfb3b871c2093ffd025c171dc70f2cc5add
Ssentamu Jordan <ssentamujordan@gmail.com>
2022-09-19 09:50:15 +0300

    coding challenge 4 with tip one done

--- js/alert.js
+++ js/alert.js
@@ -14,4 +14,21 @@ const CurrentUserName = "Jordan";
 // using prompt
 const yourName = prompt ('What is your Name ?');
 console.log(yourName)
+// coding challenge
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+const markBMI = massMark / heightMark ** 2;
+console.log( markBMI);
+
+// coding challenge 4
+const tip1 = (15/100) * 50;
+const tip2 = 15 * 300;
+const bill1 = 50 ;
+const bill2 = 300;
+const finalValue1 = tip1 + bill1;
+console.log(bill1, tip1, finalValue1)
+
+
 
