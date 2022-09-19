d0df63dfb9fe85a91238da7bf728cf17ff290b60
Ssentamu Jordan <ssentamujordan@gmail.com>
2022-09-19 09:55:24 +0300

    add a tip2 and bill2

--- js/alert.js
+++ js/alert.js
@@ -1,34 +1,37 @@
 "use script"
 // Javascript introdution
-console.log("I am JavaScript!");
-// Variable 
-const name = "John";
-const admin = "John";
-console.log(admin);
+// console.log("I am JavaScript!");
+// // Variable 
+// const name = "John";
+// const admin = "John";
+// console.log(admin);
 
-const ourPlanet = "Earth";
-const CurrentUserName = "Jordan";
+// const ourPlanet = "Earth";
+// const CurrentUserName = "Jordan";
 
-// data-types
+// // data-types
 
-// using prompt
-const yourName = prompt ('What is your Name ?');
-console.log(yourName)
-// coding challenge
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-const markBMI = massMark / heightMark ** 2;
-console.log( markBMI);
+// // using prompt
+// const yourName = prompt ('What is your Name ?');
+// console.log(yourName)
+// // coding challenge
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+// const markBMI = massMark / heightMark ** 2;
+// console.log( markBMI);
 
 // coding challenge 4
 const tip1 = (15/100) * 50;
-const tip2 = 15 * 300;
+const tip2 =(15/100) * 300;
 const bill1 = 50 ;
 const bill2 = 300;
 const finalValue1 = tip1 + bill1;
+const finalValue2 = tip2 + bill2;
 console.log(bill1, tip1, finalValue1)
+console.log(bill2, tip2, finalValue2)
+
 
 
 
