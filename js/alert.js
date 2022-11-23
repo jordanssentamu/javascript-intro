5cb87fa0f6efef18976cb686c66b8b221d2997e3
Ssentamu Jordan <ssentamujordan@gmail.com>
2022-11-23 07:58:28 +0300

    studied about conditional if and now we on loops

--- js/alert.js
+++ js/alert.js
@@ -33,8 +33,67 @@
 // console.log(bill2, tip2, finalValue2)
 
 // soln
-// test data : 275, 40 and 430
-const bill = 275;
-const tip = (bill <= 300) && (bill >= 50 ) ? bill * 0.15 : bill * 0.2;
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+// test data : (275, 40 and 430
+// const bill = 275;
+// const tip = (bill <= 300) && (bill >= 50 ) ? bill * 0.15 : bill * 0.2;
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+// function
 
+// let result = (a+b)*c;
+// console.log(result);
+  
+
+//  function getResult(a, b, c) {
+//      let result = ((a+b)*c);
+//     return result
+//  }
+//  const answer = getResult(1, 70, 4)
+//  console.log(answer)
+
+// function showMessage() {
+//     alert('Hello everyone')
+// }
+// for (let i = 0; i < 10; i = i = i + 1) {
+//     console.log (i);
+// }
+
+// task 1-deno
+// for Mark
+// const markMass = 78;
+// const markHeight = 1.69;
+// // for John
+// const johnMass = 92;
+// const johnHeight = 1.95;
+// const markBMI = markMass /  (markHeight ** 2);
+// const johnBMI = johnMass / (johnHeight ** 2);
+// console.log (markBMI);
+//  console.log(johnBMI); 
+// const markHigherBMI = markBMI;
+// const johnHigherBMI = johnBMI;
+// if (markHigherBMI > johnHigherBMI) {
+//     console.log(`Mark's BMI is greater that of John's`);
+// }else if (johnHigherBMI > markHigherBMI) {
+//     console.log(`John's BMI is greater that of Mark's`);
+
+// } else {
+//     console.log (`both equal`);
+// }
+// Test-2
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76;
+const markBMI = markMass /  (markHeight ** 2);
+const johnBMI = johnMass / (johnHeight ** 2);
+console.log (markBMI);
+ console.log(johnBMI); 
+const markHigherBMI = markBMI;
+const johnHigherBMI = johnBMI;
+if (markHigherBMI > johnHigherBMI) {
+    console.log(`Mark's BMI is greater that of John's`);
+}else if (johnHigherBMI > markHigherBMI) {
+    console.log(`John's BMI is greater that of Mark's`);
+
+} else {
+    console.log (`both are equal`);
+}
